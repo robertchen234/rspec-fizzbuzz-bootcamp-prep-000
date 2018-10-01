c19dc69026561609e3ed216f615b62e97da744dc
Robert Chen <robertchen234@gmail.com>
2018-10-01 19:11:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-  
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    print "Fizzbuzz"
+  elseif number % 3 == 0
+    print "Fizz"
+  elseif number % 5 == 0
+    print "Buzz"
+  else
+    false
 end
