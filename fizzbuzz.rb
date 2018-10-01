c269b3fc2d74f053cde5728d4d3df2c0c42a7e18
Robert Chen <robertchen234@gmail.com>
2018-10-01 19:16:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,9 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if number % 3 == 0
+  if (number % 3 == 0) && (number % 5 == 0)
+    return "Fizzbuzz"
+  elsif number % 3 == 0
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
